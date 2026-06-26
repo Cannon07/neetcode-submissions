d26ed9cca401d7e3a13374b7ff4f0561b5293c62
Jay Shitre <73459690+Cannon07@users.noreply.github.com>
2026-06-26 13:51:02 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getPossibleRow(self, matrix:List[List[int]], target: int) -> int:
+        for i in range(len(matrix)):
+            curr_row_max = matrix[i][-1]
+            if target <= curr_row_max:
+                return i
+        return len(matrix) - 1
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_index = self.getPossibleRow(matrix, target)
+        row = matrix[row_index]
+        print(row)
+        left = 0
+        right = len(row) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            if row[middle] == target:
+                return True
+            elif row[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return False
+        
